a2ddedbe53c77a9fdcf78d216264690465a4e7e5
yangnibal <wonjun9670@naver.com>
2019-10-23 22:43:38 +0900

    first commit

---
+++ board/models.py
@@ -0,0 +1,9 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+class Board(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/yang-wonjun/Desktop/외주/issue/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
