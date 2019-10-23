ca9350451db66d0ea1422b71c5dad7e20d1989e0
yangnibal <wonjun9670@naver.com>
2019-10-24 00:36:14 +0900

    post

--- board/models.py
+++ board/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.utils import timezone
 
 class Board(models.Model):
+    objects = models.Manager()
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     content = models.TextField()
---
+++ board/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('board/', views.board_list, name='board_list'),
+    path('board/<int:pk>', views.post_detail, name='post_detail'),
+    path('board/postnew', views.post_new, name='post_new'),
+    path('board/<int:pk>/edit', views.post_edit, name='post_edit')
+]
